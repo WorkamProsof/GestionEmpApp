7e64eb912ede05d061fe69348da39607e8456ec8
Alexander Arenas <arenasocampoalexander@gmail.com>
2026-05-12 14:55:05 -0500

    corrección de archivos para que concuerde con playstore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   
   // 🚀 API Configuration
-  urlContingencia:  'http://192.168.0.224:8016/dev/GestionEmp_Jhonda/',
+  // urlContingencia:  'http://192.168.0.224:8016/dev/GestionEmp_Jhonda/',
+  urlContingencia:  'http://144.217.75.8:8018/dev/GestionEmpresarial/',
   // urlBack: 'http://192.168.0.224:8016/dev/GestionEmp_Jhonda/',
   // urlBack: 'https://prosof.co:8011/dev/Gestion_Empresarial/',
   // urlBack: 'http://144.217.75.8:8018/testing/GestionEmpresarial/', // testing
